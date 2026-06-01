851b10f0d57d229e17d960494fd0deef18e014d7
obra <2132951638@qq.com>
2026-06-01 11:48:32 +0800

    fix: PDF改为GitHub Release远程加载，所有人可访问

--- utils/pdf-content.js
+++ utils/pdf-content.js
@@ -1,15 +1,14 @@
-// PDF文件URL（通过本地HTTP服务器提供）
-// 服务器已在后台启动：http://127.0.0.1:8765
+// PDF文件下载地址（托管在GitHub Release上，所有人可访问）
 const pdfUrls = {
-  ch01_01: 'http://127.0.0.1:8765/ch01_01.pdf',
-  ch01_02: 'http://127.0.0.1:8765/ch01_02.pdf',
-  ch02_01: 'http://127.0.0.1:8765/ch02_01.pdf',
-  ch03_01: 'http://127.0.0.1:8765/ch03_01.pdf',
-  ch04_01: 'http://127.0.0.1:8765/ch04_01.pdf',
-  ch05_01: 'http://127.0.0.1:8765/ch05_01.pdf',
-  ch06_01: 'http://127.0.0.1:8765/ch06_01.pdf',
-  ch07_01: 'http://127.0.0.1:8765/ch07_01.pdf',
-  ch08_01: 'http://127.0.0.1:8765/ch08_01.pdf',
+  ch01_01: 'https://github.com/666-nailong/superpowers/releases/download/pdfs-v1/ch01_01.pdf',
+  ch01_02: 'https://github.com/666-nailong/superpowers/releases/download/pdfs-v1/ch01_02.pdf',
+  ch02_01: 'https://github.com/666-nailong/superpowers/releases/download/pdfs-v1/ch02_01.pdf',
+  ch03_01: 'https://github.com/666-nailong/superpowers/releases/download/pdfs-v1/ch03_01.pdf',
+  ch04_01: 'https://github.com/666-nailong/superpowers/releases/download/pdfs-v1/ch04_01.pdf',
+  ch05_01: 'https://github.com/666-nailong/superpowers/releases/download/pdfs-v1/ch05_01.pdf',
+  ch06_01: 'https://github.com/666-nailong/superpowers/releases/download/pdfs-v1/ch06_01.pdf',
+  ch07_01: 'https://github.com/666-nailong/superpowers/releases/download/pdfs-v1/ch07_01.pdf',
+  ch08_01: 'https://github.com/666-nailong/superpowers/releases/download/pdfs-v1/ch08_01.pdf',
 };
 
 module.exports = { pdfUrls };
